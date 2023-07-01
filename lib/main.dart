811ef39c1fa80afceed76d43d03ea43ac25c26bc
govindgurjar <mr.govindbod@gmail.com>
2023-07-02 00:59:56 +0530

    WhatsApp Application First Commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:whats_app_clone/utils/num_verification.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 80,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Wellcome to WhatsApp',
+                style: TextStyle(fontSize: 36, color: Colors.green.shade900),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          Container(
+            decoration: BoxDecoration(color: Colors.transparent),
+            height: 400,
+            width: 400,
+            child: Image(image: AssetImage('images/whatsapplogo1.png')),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Text(
+              'Read our Privacy Policy. Tap "Agree and countinue" to accept the Terms of Service.',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green.shade900,
+              fixedSize: const Size(300, 45),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: ((context) => const NumVerificationPage()),
+                ),
+              );
+            },
+            child: Text('AGREE AND CONTINUE'),
+          ),
+        ],
+      ),
+    ));
+  }
+}
---
+++ lib/utils/num_verification.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NumVerificationPage extends StatefulWidget {
+  const NumVerificationPage({super.key});
+
+  @override
+  State<NumVerificationPage> createState() => _NumVerificationPageState();
+}
+
+class _NumVerificationPageState extends State<NumVerificationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
